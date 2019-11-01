8788e7c0c7604d6779ca1f3296ed1330b6f3a31d
Oliver <oliversiedler@gmx.de>
2019-11-01 18:43:50 +0100

    Implemented more readable print function to PropositionalTableauxBuilder

--- src/TableauxBuilder/PropositionalTableauxBuilder.py
+++ src/TableauxBuilder/PropositionalTableauxBuilder.py
@@ -4,6 +4,29 @@ from src.TableauxBuilder.BaseTableauxBuilder import *
 
 
 class PropositionalTableauxBuilder(BaseTableauxBuilder):
+    def print(self, indent=0):
+        tab = "\t" * indent
+        print(f"{tab}done: {self.is_done()}, closed: {self.is_closed()}")
+        if len(self.children) == 0:
+            false_e = f"\n\t\t{tab}".join([str(i) for i in self.sequent[false_exprs]])
+            true_e = f"\n\t\t{tab}".join([str(i) for i in self.sequent[true_exprs]])
+            exprs = f"{tab}exprs: [" \
+                    f"\n\t{tab}false: [\n\t\t{tab}{false_e}\n\t{tab}]," \
+                    f"\n\t{tab}true: [\n\t\t{tab}{true_e}\n\t{tab}]\n{tab}],"
+            print(exprs)
+            false_a = f"\n\t\t{tab}".join([str(i) for i in self.sequent[false_atoms]])
+            true_a = f"\n\t\t{tab}".join([str(i) for i in self.sequent[true_atoms]])
+            atoms = f"{tab}atoms: [" \
+                    f"\n\t{tab}false: [\n\t\t{tab}{false_a}\n\t{tab}]," \
+                    f"\n\t{tab}true: [\n\t\t{tab}{true_a}\n\t{tab}]\n{tab}],"
+            print(atoms)
+        print(f"{tab}children: [")
+        for child in self.children:
+            print(f"\t{tab}[")
+            child.print(indent+1)
+            print(f"\t{tab}],")
+        print(f"{tab}]")
+
     def is_done(self):
         if len(self.children) == 0:
             for true_atom in self.sequent[true_atoms]:
