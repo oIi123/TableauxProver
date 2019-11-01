6c8668b5fc411d202663c68fec28149f542def22
Oliver <oliversiedler@gmx.de>
2019-11-01 11:41:00 +0100

    Refactored sequent dictionary -> key-strings are now constants

--- src/TableauxBuilder/PropositionalTableauxBuilder.py
+++ src/TableauxBuilder/PropositionalTableauxBuilder.py
@@ -2,6 +2,11 @@ from src.Model.FoplExpressionTree import Expr, Not, And, Impl, Or, Eq
 from src.Model.PropositionalExpressionTree import Atom
 import copy
 
+false_exprs = "false"
+true_exprs = "true"
+false_atoms = "false_atoms"
+true_atoms = "true_atoms"
+
 
 class PropositionalTableauxBuilder:
     visiting_false = True
@@ -11,20 +16,20 @@ class PropositionalTableauxBuilder:
             self.sequent = sequent
         else:
             self.sequent = {
-                "false": [expr],
-                "true": [],
-                "false_atoms": [],
-                "true_atoms": []
+                false_exprs: [expr],
+                true_exprs: [],
+                false_atoms: [],
+                true_atoms: [],
             }
         self.done = False
         self.children = []
 
     def is_done(self):
         if len(self.children) == 0:
-            for true_atom in self.sequent["true_atoms"]:
-                if true_atom in self.sequent["false_atoms"]:
+            for true_atom in self.sequent[true_atoms]:
+                if true_atom in self.sequent[false_atoms]:
                     return True
-            return len(self.sequent["false"]) == len(self.sequent["true"]) == 0
+            return len(self.sequent[false_exprs]) == len(self.sequent[true_exprs]) == 0
         else:
             for child in self.children:
                 if not child.is_done():
@@ -32,8 +37,8 @@ class PropositionalTableauxBuilder:
             return True
 
     def __str__(self):
-        exprs = f"exprs: [false: {[str(i) for i in self.sequent['false']]} true: {[str(i) for i in self.sequent['true']]}]\n"
-        atoms = f"atoms: [false: {[str(i) for i in self.sequent['false_atoms']]} true: {[str(i) for i in self.sequent['true_atoms']]}]\n"
+        exprs = f"exprs: [false: {[str(i) for i in self.sequent[false_exprs]]} true: {[str(i) for i in self.sequent[true_exprs]]}]\n"
+        atoms = f"atoms: [false: {[str(i) for i in self.sequent[false_atoms]]} true: {[str(i) for i in self.sequent[true_atoms]]}]\n"
         children = f"children: [{[str(i) for i in self.children]}]\n"
         return exprs + atoms + children
 
@@ -44,62 +49,62 @@ class PropositionalTableauxBuilder:
                     child.visit()
                     return
             return
-        if len(self.sequent["false"]) > 0:
+        if len(self.sequent[false_exprs]) > 0:
             self.visiting_false = True
-            expr = self.sequent["false"][-1]
+            expr = self.sequent[false_exprs][-1]
             expr.visit(self)
-            self.sequent["false"].remove(expr)
+            self.sequent[false_exprs].remove(expr)
         else:
             self.visiting_false = False
-            expr = self.sequent["true"][-1]
+            expr = self.sequent[true_exprs][-1]
             expr.visit(self)
-            self.sequent["true"].remove(expr)
+            self.sequent[true_exprs].remove(expr)
 
     def visited_Atom(self, atom: Atom):
-        self.sequent["false_atoms" if self.visiting_false else "true_atoms"].append(atom)
+        self.sequent[false_atoms if self.visiting_false else true_atoms].append(atom)
 
     def visited_Not(self, n: Not):
-        self.sequent["true" if self.visiting_false else "false"].append(n.expr)
+        self.sequent[true_exprs if self.visiting_false else false_exprs].append(n.expr)
 
     def visited_And(self, a: And):
         if self.visiting_false:
             lhs = PropositionalTableauxBuilder(sequent=copy.deepcopy(self.sequent))
             rhs = PropositionalTableauxBuilder(sequent=copy.deepcopy(self.sequent))
-            lhs.sequent["false"].remove(a)
-            rhs.sequent["false"].remove(a)
-            lhs.sequent["false"].append(a.lhs)
-            rhs.sequent["false"].append(a.rhs)
+            lhs.sequent[false_exprs].remove(a)
+            rhs.sequent[false_exprs].remove(a)
+            lhs.sequent[false_exprs].append(a.lhs)
+            rhs.sequent[false_exprs].append(a.rhs)
             self.children.append(lhs)
             self.children.append(rhs)
         else:
-            self.sequent["true"].append(a.lhs)
-            self.sequent["true"].append(a.rhs)
+            self.sequent[true_exprs].append(a.lhs)
+            self.sequent[true_exprs].append(a.rhs)
 
     def visited_Or(self, o: Or):
         if self.visiting_false:
-            self.sequent["false"].append(o.lhs)
-            self.sequent["false"].append(o.rhs)
+            self.sequent[false_exprs].append(o.lhs)
+            self.sequent[false_exprs].append(o.rhs)
         else:
             lhs = PropositionalTableauxBuilder(sequent=copy.deepcopy(self.sequent))
             rhs = PropositionalTableauxBuilder(sequent=copy.deepcopy(self.sequent))
-            lhs.sequent["true"].remove(o)
-            rhs.sequent["true"].remove(o)
-            lhs.sequent["true"].append(o.lhs)
-            rhs.sequent["true"].append(o.rhs)
+            lhs.sequent[true_exprs].remove(o)
+            rhs.sequent[true_exprs].remove(o)
+            lhs.sequent[true_exprs].append(o.lhs)
+            rhs.sequent[true_exprs].append(o.rhs)
             self.children.append(lhs)
             self.children.append(rhs)
 
     def visited_Impl(self, impl: Impl):
         if self.visiting_false:
-            self.sequent["false"].append(impl.lhs)
-            self.sequent["true"].append(impl.rhs)
+            self.sequent[false_exprs].append(impl.lhs)
+            self.sequent[true_exprs].append(impl.rhs)
         else:
             lhs = PropositionalTableauxBuilder(sequent=copy.deepcopy(self.sequent))
             rhs = PropositionalTableauxBuilder(sequent=copy.deepcopy(self.sequent))
-            lhs.sequent["true"].remove(impl)
-            rhs.sequent["true"].remove(impl)
-            lhs.sequent["false"].append(impl.lhs)
-            rhs.sequent["true"].append(impl.rhs)
+            lhs.sequent[true_exprs].remove(impl)
+            rhs.sequent[true_exprs].remove(impl)
+            lhs.sequent[false_exprs].append(impl.lhs)
+            rhs.sequent[true_exprs].append(impl.rhs)
             self.children.append(lhs)
             self.children.append(rhs)
 
@@ -107,24 +112,24 @@ class PropositionalTableauxBuilder:
         if self.visiting_false:
             lhs = PropositionalTableauxBuilder(sequent=copy.deepcopy(self.sequent))
             rhs = PropositionalTableauxBuilder(sequent=copy.deepcopy(self.sequent))
-            lhs.sequent["false"].remove(eq)
-            rhs.sequent["false"].remove(eq)
+            lhs.sequent[false_exprs].remove(eq)
+            rhs.sequent[false_exprs].remove(eq)
 
-            lhs.sequent["false"].append(eq.lhs)
-            lhs.sequent["true"].append(eq.rhs)
+            lhs.sequent[false_exprs].append(eq.lhs)
+            lhs.sequent[true_exprs].append(eq.rhs)
 
-            rhs.sequent["true"].append(eq.lhs)
-            rhs.sequent["false"].append(eq.rhs)
+            rhs.sequent[true_exprs].append(eq.lhs)
+            rhs.sequent[false_exprs].append(eq.rhs)
             self.children.append(lhs)
             self.children.append(rhs)
         else:
             lhs = PropositionalTableauxBuilder(sequent=copy.deepcopy(self.sequent))
             rhs = PropositionalTableauxBuilder(sequent=copy.deepcopy(self.sequent))
-            lhs.sequent["true"].remove(eq)
-            rhs.sequent["true"].remove(eq)
-            lhs.sequent["true"].append(eq.lhs)
-            lhs.sequent["true"].append(eq.rhs)
-            rhs.sequent["false"].append(eq.lhs)
-            rhs.sequent["false"].append(eq.rhs)
+            lhs.sequent[true_exprs].remove(eq)
+            rhs.sequent[true_exprs].remove(eq)
+            lhs.sequent[true_exprs].append(eq.lhs)
+            lhs.sequent[true_exprs].append(eq.rhs)
+            rhs.sequent[false_exprs].append(eq.lhs)
+            rhs.sequent[false_exprs].append(eq.rhs)
             self.children.append(lhs)
             self.children.append(rhs)
