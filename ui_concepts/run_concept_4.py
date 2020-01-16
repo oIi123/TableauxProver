1b1297fab26b29a236b0ab60b9a0c19718104665
oIi123 <49989913+oIi123@users.noreply.github.com>
2020-01-16 08:44:42 +0100

    gui concept 2 and documentation of concepts

---
+++ ui_concepts/run_concept_4.py
@@ -0,0 +1,108 @@
+import sys
+from typing import Union, Optional
+
+import PySide2
+from PySide2.QtGui import QPainter, QPaintEvent, QPen
+from PySide2.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QTextEdit
+from PySide2.QtCore import QFile
+from ui_concepts.ui_mainwindow import Ui_MainWindow
+from ui_concepts.ui_input_window import Ui_MainWindow as Ui_InputWindow
+
+
+def get_child_widget(parent: QWidget, name: str) -> Union[Optional[QWidget], None]:
+    for child in parent.children():
+        if child.objectName() == name:
+            return child
+        sub_child = get_child_widget(child, name)
+        if sub_child is not None:
+            return sub_child
+
+    return None
+
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.scroll_area_content = get_child_widget(self, "scrollAreaWidgetContents")
+        self.scroll_area_content.installEventFilter(self)
+
+        # 550, 325, 550, 480
+        # 925, 325, 925, 480
+        geoms = [
+            (375, 350, 150, 130),
+            (575, 350, 150, 130),
+            (775, 350, 150, 130),
+            (975, 350, 150, 130),
+        ]
+        for geom in geoms:
+            text_input = QTextEdit(parent=self.scroll_area_content)
+            text_input.setGeometry(*geom)
+            text_input.show()
+
+        geoms = [
+            (525, 480, 50, 50),
+            (925, 480, 50, 50),
+        ]
+        for geom in geoms:
+            btn = QPushButton(parent=self.scroll_area_content)
+            btn.setGeometry(*geom)
+            btn.setStyleSheet("QPushButton {qproperty-icon: url(add.svg); background-color: rgb(255,255,255,0); qproperty-iconSize: 24px;}")
+            btn.show()
+
+    def eventFilter(self, watched: PySide2.QtCore.QObject, event: PySide2.QtCore.QEvent):
+        if watched is self.scroll_area_content and type(event) is QPaintEvent:
+            normal_font = PySide2.QtGui.QFont('MS Shell Dlg 2', 14)
+            underlined_font = PySide2.QtGui.QFont('MS Shell Dlg 2', 14)
+            underlined_font.setUnderline(True)
+
+            normal_pen = QPen(PySide2.QtGui.QColor(0, 0, 0))
+            dotted_pen = QPen(normal_pen)
+            dotted_pen.setStyle(PySide2.QtCore.Qt.DashDotDotLine)
+
+            p = QPainter()
+            p.begin(self.scroll_area_content)
+            p.setPen(normal_pen)
+            p.setFont(normal_font)
+
+            p.drawLine(250, 75, 500, 75)
+            p.drawLine(375, 25, 375, 250)
+            p.drawLine(200, 250, 550, 250)
+            p.drawLine(200, 250, 200, 400)
+            p.drawLine(550, 250, 550, 325)
+
+            p.drawLine(175, 400, 225, 400)
+
+            p.drawText(312, 50, "T")
+            p.drawText(437, 50, "F")
+
+            p.drawText(400, 170, "A")
+            p.drawText(150, 350, "A")
+
+            p.setFont(underlined_font)
+            p.drawText(400, 120, "A|!A&(B&!C)")
+            p.drawText(400, 220, "!A&(B&!C)")
+            p.drawText(225, 300, "!A")
+            p.drawText(575, 300, "B&!C")
+
+            p.setPen(dotted_pen)
+            p.drawLine(550, 325, 550, 480)
+            p.drawLine(550, 325, 950, 325)
+            p.drawLine(950, 325, 950, 480)
+
+            p.end()
+
+            self.scroll_area_content.setMinimumSize(1150, 550)
+
+        return False
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = Main()
+    window.show()
+
+    sys.exit(app.exec_())
--- ui_concepts/ui_mainwindow.py
+++ ui_concepts/ui_mainwindow.py
@@ -19,29 +19,66 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(800, 630)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.horizontalLayout = QHBoxLayout()
+        self.horizontalGroupBox = QGroupBox(self.centralwidget)
+        self.horizontalGroupBox.setObjectName(u"horizontalGroupBox")
+        self.horizontalLayout_2 = QHBoxLayout(self.horizontalGroupBox)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.radioButton_4 = QRadioButton(self.horizontalGroupBox)
+        self.radioButton_4.setObjectName(u"radioButton_4")
+        self.radioButton_4.setMinimumSize(QSize(0, 0))
+        self.radioButton_4.setChecked(True)
+
+        self.horizontalLayout_2.addWidget(self.radioButton_4)
+
+        self.radioButton_3 = QRadioButton(self.horizontalGroupBox)
+        self.radioButton_3.setObjectName(u"radioButton_3")
+        self.radioButton_3.setMinimumSize(QSize(0, 0))
+
+        self.horizontalLayout_2.addWidget(self.radioButton_3)
+
+        self.radioButton_2 = QRadioButton(self.horizontalGroupBox)
+        self.radioButton_2.setObjectName(u"radioButton_2")
+        self.radioButton_2.setMinimumSize(QSize(0, 0))
+
+        self.horizontalLayout_2.addWidget(self.radioButton_2)
+
+        self.radioButton = QRadioButton(self.horizontalGroupBox)
+        self.radioButton.setObjectName(u"radioButton")
+
+        self.horizontalLayout_2.addWidget(self.radioButton)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer_2)
+
+
+        self.verticalLayout_2.addWidget(self.horizontalGroupBox)
+
+        self.groupBox = QGroupBox(self.centralwidget)
+        self.groupBox.setObjectName(u"groupBox")
+        self.horizontalLayout = QHBoxLayout(self.groupBox)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setSizeConstraint(QLayout.SetDefaultConstraint)
-        self.automatic_radio_btn = QRadioButton(self.centralwidget)
+        self.automatic_radio_btn = QRadioButton(self.groupBox)
         self.automatic_radio_btn.setObjectName(u"automatic_radio_btn")
         self.automatic_radio_btn.setMinimumSize(QSize(100, 0))
         self.automatic_radio_btn.setChecked(False)
 
         self.horizontalLayout.addWidget(self.automatic_radio_btn)
 
-        self.manual_radio_btn = QRadioButton(self.centralwidget)
+        self.manual_radio_btn = QRadioButton(self.groupBox)
         self.manual_radio_btn.setObjectName(u"manual_radio_btn")
         self.manual_radio_btn.setMinimumSize(QSize(70, 0))
         self.manual_radio_btn.setChecked(True)
 
         self.horizontalLayout.addWidget(self.manual_radio_btn)
 
-        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton = QPushButton(self.groupBox)
         self.pushButton.setObjectName(u"pushButton")
         self.pushButton.setMinimumSize(QSize(20, 20))
         self.pushButton.setMaximumSize(QSize(20, 20))
@@ -64,7 +101,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout.addItem(self.horizontalSpacer)
 
 
-        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.verticalLayout_2.addWidget(self.groupBox)
 
         self.scrollArea = QScrollArea(self.centralwidget)
         self.scrollArea.setObjectName(u"scrollArea")
@@ -77,6 +114,15 @@ class Ui_MainWindow(object):
         self.scrollAreaWidgetContents.setMinimumSize(QSize(500, 500))
         self.scrollAreaWidgetContents.setStyleSheet(u"#scrollAreaWidgetContents {\n"
 "	background-color: rgb(255, 255, 255);\n"
+"}")
+        self.pushButton_2 = QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(5, 5, 31, 28))
+        self.pushButton_2.setStyleSheet(u"#pushButton_2 {\n"
+"	background-color: rgba(255, 255, 255, 0);\n"
+"	border-color: rgba(255, 255, 255, 0);\n"
+"	color: rgba(255, 255, 255, 0);\n"
+"	qproperty-icon: url(stornieren.svg);\n"
 "}")
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
 
@@ -101,6 +147,22 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Tableaux Prover", None))
+#if QT_CONFIG(tooltip)
+        self.radioButton_4.setToolTip(QCoreApplication.translate("MainWindow", u"Propositional Logic", None))
+#endif // QT_CONFIG(tooltip)
+        self.radioButton_4.setText(QCoreApplication.translate("MainWindow", u"PL", None))
+#if QT_CONFIG(tooltip)
+        self.radioButton_3.setToolTip(QCoreApplication.translate("MainWindow", u"First Order Predicate Logic", None))
+#endif // QT_CONFIG(tooltip)
+        self.radioButton_3.setText(QCoreApplication.translate("MainWindow", u"FOPL", None))
+#if QT_CONFIG(tooltip)
+        self.radioButton_2.setToolTip(QCoreApplication.translate("MainWindow", u"Intuitionistic Propositional Logic", None))
+#endif // QT_CONFIG(tooltip)
+        self.radioButton_2.setText(QCoreApplication.translate("MainWindow", u"IPL", None))
+#if QT_CONFIG(tooltip)
+        self.radioButton.setToolTip(QCoreApplication.translate("MainWindow", u"Intuitionistic First Order Predicate Logic", None))
+#endif // QT_CONFIG(tooltip)
+        self.radioButton.setText(QCoreApplication.translate("MainWindow", u"IFOPL", None))
         self.automatic_radio_btn.setText(QCoreApplication.translate("MainWindow", u"Automatic", None))
         self.manual_radio_btn.setText(QCoreApplication.translate("MainWindow", u"Manual", None))
 #if QT_CONFIG(tooltip)
@@ -110,5 +172,6 @@ class Ui_MainWindow(object):
 #if QT_CONFIG(tooltip)
         self.scrollArea.setToolTip("")
 #endif // QT_CONFIG(tooltip)
+        self.pushButton_2.setText("")
     # retranslateUi
 
