e6d38c5099d8c3467669442a274f3e60e38e7ae9
oIi123 <49989913+oIi123@users.noreply.github.com>
2020-03-02 11:14:52 +0100

    removed unnecessary imports

--- src/main.py
+++ src/main.py
@@ -1,13 +1,5 @@
-from src.Model.PropositionalExpressionTree import Atom
-from src.Parser.FoplParser import FoplParser
-from src.Parser.PropParser import PropParser
-from src.TableauxBuilder.FoplTableauxBuilder import FoplTableauxBuilder
-from src.TableauxBuilder.IpcTableauxBuilder import IpcTableauxBuilder
-from src.TableauxBuilder.PropositionalTableauxBuilder import PropositionalTableauxBuilder
 from PySide2.QtWidgets import QApplication
-
 from src.view.MainWindow import MainWindow
-
 import sys
 
 if __name__ == '__main__':
