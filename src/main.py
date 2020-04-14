a7858641ac0b18b7f96063edc062d1251fc2e48b
Oliver <oliversiedler@gmx.de>
2020-04-14 09:50:07 +0200

    using font from ttf file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from PySide2.QtWidgets import QApplication
+from PySide2.QtGui import QFontDatabase
 from src.view.MainWindow import MainWindow
 import sys
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    QFontDatabase.addApplicationFont('res/YuGothicUI Semibold.ttf')
 
     window = MainWindow()
     window.show()
